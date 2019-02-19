abc55b8b200038525487fad0a15140b952cfe160
ernesto0 <30987669+ernesto0@users.noreply.github.com>
2019-02-19 11:09:59 -0600

    modified directory structure and refactored app.js for improved modularity

--- android/TandaPay/App.js
+++ android/TandaPay/App.js
@@ -1,69 +1,19 @@
 import React from 'react';
 import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+import startScreen from './src/screens/startScreen';
+import loginScreen from './src/screens/loginScreen';
+import registerScreen from './src/screens/registerScreen';
 
-class startScreen extends React.Component {
 
-  _onPressCreateAccount() {
-    Alert.alert('BRING UP CREATE ACCOUNT PAGE')
-  }
 
-  _onPressLogIn() {
-    Alert.alert('BRING UP LOG IN')
-  }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.bigBlue}>TandaPay</Text>
-        <View style={styles.buttonContainer}>         
-          <Button
-             onPress={() => this.props.navigation.navigate('Login')}
-            title="Create Account"
-          />
-        </View>
-        <View style={styles.buttonContainer}>         
-          <Button
-            onPress={() => this.props.navigation.navigate('Login')}
-            title="Log In"
-          />
-        </View>
-      </View>
-    );
-  }
-}
 
-class logInScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Welcome to the Log In Screen</Text>
-        <Text>Username: </Text>
-        <Text>Password:</Text>
-      </View>
-    );
-  }
-}
-
-class registerScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Welcome to the Register Screen</Text>
-        <Text>Email: </Text>
-        <Text>First Name:</Text>
-        <Text>Last Name:</Text>
-        <Text>Username: </Text>
-        <Text>Password:</Text>
-      </View>
-    );
-  }
-}
 
 const RootStack = createStackNavigator(
   {
     Start: startScreen,
-    Login: logInScreen,
+    Login: loginScreen,
     Register: registerScreen, 
   },
   {
@@ -80,25 +30,4 @@ export default class App extends React.Component {
 
 const AppContainer = createAppContainer(RootStack);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bigBlue: {
-    color: '#5f9ea0',
-    fontWeight: 'bold',
-    fontSize: 70,
-  },
-  bigGrey: {
-    color: '#A9A9A9',
-    fontSize: 20,
-  },
-  buttonContainer: {
-    margin: 10,
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-});
+
---
+++ android/TandaPay/src/screens/registerScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+export default class registerScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Welcome to the Register Screen</Text>
+          <Text>Email: </Text>
+          <Text>First Name:</Text>
+          <Text>Last Name:</Text>
+          <Text>Username: </Text>
+          <Text>Password:</Text>
+        </View>
+      );
+    }
+  }
