316bb14202a1936c2a397448657b87a27a79a4bb
franco <francolynn@gmail.com>
2019-02-19 21:18:49 -0600

    added trustwallet open/install feature

--- android/TandaPay/src/screens/startScreen.js
+++ android/TandaPay/src/screens/startScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Alert, AppRegistry, Button, StyleSheet, Text, View } from 'react-native';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { Linking } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -27,12 +28,17 @@ const styles = StyleSheet.create({
 
 export default class startScreen extends React.Component {
 
-  _onPressCreateAccount() {
-    Alert.alert('BRING UP CREATE ACCOUNT PAGE')
-  }
-
-  _onPressLogIn() {
-    Alert.alert('BRING UP LOG IN')
+  _onPressWallet() {
+    url = 'https://links.trustwalletapp.com/a/key_live_lfvIpVeI9TFWxPCqwU8rZnogFqhnzs4D?&event=openURL&url=https://google.com';
+    Linking.canOpenURL(url)
+    .then((supported) => {
+      if (!supported) {
+        console.log("Can't handle url: " + url);
+      } else {
+        return Linking.openURL(url);
+      }
+    })
+    .catch((err) => console.error('An error occurred', err));
   }
 
   render() {
@@ -41,17 +47,11 @@ export default class startScreen extends React.Component {
         <Text style={styles.bigBlue}>TandaPay</Text>
         <View style={styles.buttonContainer}>         
           <Button
-             onPress={() => this.props.navigation.navigate('Login')}
-            title="Create Account"
-          />
-        </View>
-        <View style={styles.buttonContainer}>         
-          <Button
-            onPress={() => this.props.navigation.navigate('Login')}
-            title="Log In"
+             onPress={() => this._onPressWallet()}
+            title="Wallet"
           />
         </View>
       </View>
-    );
+    );``
   }
 }
