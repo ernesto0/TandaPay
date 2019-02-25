54d8885c56ada20cf298b426d7a81312e0de9d86
Haneya Khan <haneyakhan@cometnet-10-21-31-90.utdallas.edu>
2019-02-25 15:45:34 -0600

    Update register code

--- android/TandaPay/src/screens/StartScreen.js
+++ android/TandaPay/src/screens/StartScreen.js
@@ -4,44 +4,87 @@ import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
 import { Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const Tanda = t.struct({
+  email: t.String,
+  code: t.String
+});
+
+const formStyles = {
+    ...Form.stylesheet,
+    formGroup: {
+      normal: {
+        marginBottom: 10
+      },
+    },
+    controlLabel: {
+      normal: {
+        color: 'blue',
+        fontSize: 18,
+        marginBottom: 7,
+        fontWeight: '600'
+      },
+      // the style applied when a validation error occours
+      error: {
+        color: 'red',
+        fontSize: 18,
+        marginBottom: 7,
+        fontWeight: '600'
+      }
+    }
+  }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   tandaLogo: {
     color: '#5f9ea0',
     fontWeight: 'bold',
     fontSize: 70,
     alignItems: 'center',
     justifyContent: 'center'
-  },
+  }
 });
+
+const options = {
+    fields: {
+      email: {
+        error: ''
+      },
+      code: {
+        error: ''
+      }
+    },
+    stylesheet: formStyles,
+  };
 
 export default class StartScreen extends React.Component {
 
   constructor(props){
     super(props)
 
-    this.state = {
-      code: ''
-    }
+    // this.state = {
+    //   tanda = const
+    // }
   }
   
   _onPressSubmit() {
-    this.props.navigation.navigate('Register')
+    const value = this._form.getValue();
+    this.setState({value});
+    // console.log('value: ', value);
     console.log(this.state.code);
+    this.props.navigation.navigate('Register')
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.tandaLogo}>TandaPay</Text>
-        <TextInput
-          placeholder="Enter code here."
-          onChangeText={(text) => this.setState({code:text})}
+        <Form 
+          ref={c => this._form = c}
+          type={Tanda} 
+          options={options}
         />
         <Button
           title="Submit Code"
