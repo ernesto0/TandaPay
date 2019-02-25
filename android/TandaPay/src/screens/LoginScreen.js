e3fdd0de933d032d6f834ee7ad22c4dedbf855bf
Haneya Khan <haneyakhan@cometnet-10-21-31-90.utdallas.edu>
2019-02-25 11:54:00 -0600

    Made git case sensitive to recognize file name changes

---
+++ android/TandaPay/src/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+export default class logInScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Welcome to the Log In Screen</Text>
+          <Text>Username: </Text>
+          <Text>Password:</Text>
+        </View>
+      );
+    }
+}
