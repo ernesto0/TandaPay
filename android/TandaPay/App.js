e9616731916f2fe19fcb0244445e4511c8f61e59
Haneya Khan <haneyakhan@cometnet-10-21-61-179.utdallas.edu>
2019-02-26 16:32:55 -0600

    Build screen for displaying members and status

--- android/TandaPay/App.js
+++ android/TandaPay/App.js
@@ -4,25 +4,19 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 import StartScreen from './src/screens/StartScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-
-
-
-
-const style ={
-
-}
+import StatusScreen from './src/screens/StatusScreen';
 
 const RootStack = createStackNavigator(
   {
     Start: StartScreen,
     Login: LoginScreen,
     Register: RegisterScreen, 
+    Status: StatusScreen
   },
   {
     initialRouteName: 'Start',
   }
 );
-
 
 export default class App extends React.Component {
   render() {
