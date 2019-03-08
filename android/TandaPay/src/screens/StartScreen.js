20928aca35f00791ce5edc0e2a9177a4357f0d6d
tdcnguyen96 <35542327+NugyenDs@users.noreply.github.com>
2019-03-08 15:12:37 -0600

    changes to the screens

--- android/TandaPay/src/screens/StartScreen.js
+++ android/TandaPay/src/screens/StartScreen.js
@@ -3,61 +3,6 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 
 import t from 'tcomb-form-native';
 
-const Form = t.form.Form;
-
-const Tanda = t.struct({
-  email: t.String,
-  code: t.String
-});
-
-const formStyles = {
-    ...Form.stylesheet,
-    formGroup: {
-      normal: {
-        marginBottom: 10
-      },
-    },
-    controlLabel: {
-      normal: {
-        color: 'blue',
-        fontSize: 18,
-        marginBottom: 7,
-        fontWeight: '600'
-      },
-      // the style applied when a validation error occours
-      error: {
-        color: 'red',
-        fontSize: 18,
-        marginBottom: 7,
-        fontWeight: '600'
-      }
-    }
-  }
-
-const styles = StyleSheet.create({
-  tandaLogo: {
-    color: '#5f9ea0',
-    fontWeight: 'bold',
-    fontSize: 70,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
-
-const options = {
-    fields: {
-      email: {
-        error: ''
-      },
-      code: {
-        error: ''
-      }
-    },
-    stylesheet: formStyles,
-  };
-
-export default class StartScreen extends React.Component {
-
   constructor(props){
     super(props)
 
