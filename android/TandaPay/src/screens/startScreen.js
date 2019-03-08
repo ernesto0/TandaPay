3b56188ad5b616db2c1f0f7840f7d42fa8e81a55
tdcnguyen96 <35542327+NugyenDs@users.noreply.github.com>
2019-03-08 15:07:11 -0600

    fixed login screen

--- android/TandaPay/src/screens/startScreen.js
+++ android/TandaPay/src/screens/startScreen.js
@@ -1,95 +1,92 @@
 import React from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import {StyleSheet, View, TextInput, Text, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
 
-import t from 'tcomb-form-native';
-
-const Form = t.form.Form;
-
-const Tanda = t.struct({
-  email: t.String,
-  code: t.String
-});
-
-const formStyles = {
-    ...Form.stylesheet,
-    formGroup: {
-      normal: {
-        marginBottom: 10
-      },
-    },
-    controlLabel: {
-      normal: {
-        color: 'blue',
-        fontSize: 18,
-        marginBottom: 7,
-        fontWeight: '600'
-      },
-      // the style applied when a validation error occours
-      error: {
-        color: 'red',
-        fontSize: 18,
-        marginBottom: 7,
-        fontWeight: '600'
-      }
-    }
-  }
-
-const styles = StyleSheet.create({
-  tandaLogo: {
-    color: '#5f9ea0',
-    fontWeight: 'bold',
-    fontSize: 70,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
-
-const options = {
-    fields: {
-      email: {
-        error: ''
-      },
-      code: {
-        error: ''
-      }
-    },
-    stylesheet: formStyles,
-  };
-
-export default class StartScreen extends React.Component {
+export default class LoginScreen extends React.Component {
 
   constructor(props){
     super(props)
 
     this.state = {
-      value: {
-        email: '',
-        code: ''
-      },
+      username: '',
+      code: ''
     }
   }
-  
+
   _onPressSubmit() {
-    const value = this._form.getValue();
-    console.log('value: ', value);
-    
-    this.props.navigation.navigate('Login')
+      console.log(this.state.username);
+      console.log(this.state.password);
+      
+      fetch('http://10.122.167.191:5000/api/tanda/test')
+      .then(response => {
+      return response.json();
+      }).then(response => {
+      // this.setState({resp: data})
+      console.log('resp: ', response);
+      }).catch((error) => {
+      console.log(error)
+      })
+
+      this.props.navigation.navigate('Login');
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.tandaLogo}>TandaPay</Text>
-        <Form 
-          ref={c => this._form = c}
-          type={Tanda} 
-          options={options}
-        />
-        <Button
-          title="Submit Code"
-          onPress={() => this._onPressSubmit()}
-          />
+      <View style={style.container}>
+        <KeyboardAvoidingView style={style.inputContainer} behavior="padding" >
+              <TextInput 
+                placeholder="example@email.com"
+                placeholderTextColor="#fff" 
+                style={style.input} 
+                onChangeText={(text) => this.setState({username:text})}
+              />
+              <TextInput 
+                placeholder="tanda code"
+                placeholderTextColor="#fff" 
+                style={style.input} 
+                secureTextEntry={true}
+                onChangeText={(text) => this.setState({password:text})}
+              />
+              <TouchableOpacity style = {style.buttonContainer}
+                onPress={() => this._onPressSubmit()}>
+                <Text style = {style.buttonText}>
+                    ENTER
+                </Text>
+              </TouchableOpacity>
+        </KeyboardAvoidingView>
       </View>
     );
   }
-}
+  }
+  
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: '#00cec9',
+    },
+    inputContainer:{
+      flex: 3,
+      justifyContent:'center',
+      alignItems: 'stretch',
+    },
+    container2: {
+      padding:20
+    },
+    input:{
+      height: 40,
+      marginBottom: 20,
+      color: '#fff',
+      paddingHorizontal: 10,
+      fontWeight: '700',
+      borderBottomColor: '#fdcb6e', // Add this to specify bottom border color
+      borderBottomWidth: 2     // Add this to specify bottom border thickness
+    },
+    buttonContainer:{
+        backgroundColor: '#fdcb6e',
+        paddingVertical: 10,
+    },
+    buttonText:{
+        textAlign: 'center',
+        color:'#FFF',
+    }
+  });
